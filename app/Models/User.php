98e2adb4fcccf810441b509d550aaf33024d95bf
Richie-Z <zakariarichie@yahoo.com>
2021-08-19 11:33:48 +0700

    soft delete user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,12 +7,13 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Lumen\Auth\Authorizable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use Authenticatable, Authorizable, HasFactory, SoftDeletes;
     protected $fillable = [
         'username', 'email', 'password', 'is_verified'
     ];
